11ee1695c735803c7053f4c4010773fd7e2e9a88
VladimirMynka <vladimirmynka@yandex.ru>
2023-06-08 09:29:43 +0700

    Resnet18 realization

---
+++ src/train/models/resnet18_realization/model.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+
+import src.train.models.resnet18_realization.model_layers as layers
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        self.image_decoder = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            layers.BaseLayer(64, 64),
+            layers.BaseLayer(64, 64),
+
+            layers.BaseLayerWithDownSample(64, 128),
+            layers.BaseLayer(128, 128),
+
+            layers.BaseLayerWithDownSample(128, 256),
+            layers.BaseLayer(256, 256),
+
+            layers.BaseLayerWithDownSample(256, 512),
+            layers.BaseLayer(512, 512),
+
+            nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        )
+
+        self.classifier = nn.Linear(512, 2)
+
+    def forward(self, x: torch.Tensor):
+        return self.classifier(self.image_decoder(x))
